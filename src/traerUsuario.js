0fc9e8cc8d9b684ad9f0b2b5d8476adbc3427bed
jaen <jaenfigueroa03042000@gmail.com>
2023-07-19 16:15:22 -0500

    feat: add more functions

--- src/traerUsuario.js
+++ src/traerUsuario.js
@@ -1,13 +1,49 @@
-const { Octokit } = require('@octokit/rest');
+const { Octokit } = require('octokit');
 require('dotenv').config();
 
 const octokit = new Octokit({
   auth: process.env.ACCESS_TOKEN,
 })
 
-const getUser = async () => {
-  const data = await octokit.request('/user');
+// TRAER DATOS DEL USUARIO AUTENTICADO
+const getUserAuthenticated = async () => {
+  const data = await octokit.rest.users.getAuthenticated();
+  console.log(data);
+}
+
+//CREAR UN REPOSITORIO NUEVO
+const createNewRepo = async (repoName) => {
+  const data = await octokit.rest.repos.createForAuthenticatedUser({
+    name: repoName,
+    private: true,
+    description: 'Este es un repositorio creado desde la API de GitHub',
+    has_discussions: true,
+    delete_branch_on_merge: true,
+  })
+
   console.log(data)
 }
 
-getUser();
+//CREAR UN NUEVO FORK EN UN REPOSITORIO
+const createNewFork = async () => {
+  const data = await octokit.rest.repos.createFork({
+    owner: 'yashraj24october',
+    repo: 'My-portfolio-API',
+  })
+
+  console.log(data)
+}
+
+//CREAR UN ISSUE EN UN REPOSITORIO
+const createNewIssue = async () => {
+  const data = await octokit.rest.issues.create({
+    owner: 'jaenfigueroa',
+    repo: 'repo-prueba25',
+    title: 'Este es un nuevo Issue',
+    body: 'Este es el cuerpo de un issue',
+    labels: ['bug'],
+    assignees: ['jaenfigueroa'],
+  })
+
+  console.log(data)
+}
